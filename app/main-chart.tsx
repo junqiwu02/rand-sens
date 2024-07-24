968679fc714aa062b641100eb99eca0b53f30b38
Junqi <junqiwu02@gmail.com>
2024-07-24 01:12:09 -0400

    UI mockup

---
+++ app/main-chart.tsx
@@ -0,0 +1,89 @@
+"use client";
+
+import { GitCommitVertical, TrendingUp } from "lucide-react";
+import { Area, AreaChart, CartesianGrid, XAxis } from "recharts";
+
+import {
+  ChartConfig,
+  ChartContainer,
+  ChartTooltip,
+  ChartTooltipContent,
+} from "@/components/ui/chart";
+const chartData = [
+  { month: "January", desktop: 186 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+  { month: "February", desktop: 305 },
+  { month: "March", desktop: 237 },
+  { month: "April", desktop: 73 },
+  { month: "May", desktop: 209 },
+];
+
+const chartConfig = {
+  desktop: {
+    label: "Desktop",
+    color: "hsl(var(--chart-1))",
+  },
+} satisfies ChartConfig;
+
+export function MainChart() {
+  return (
+    <ChartContainer config={chartConfig}>
+      <AreaChart
+        accessibilityLayer
+        data={chartData}
+        margin={{
+          left: 12,
+          right: 12,
+        }}
+      >
+        <CartesianGrid vertical={false} />
+        <XAxis
+          dataKey="month"
+          tickLine={false}
+          axisLine={false}
+          tickMargin={8}
+          tickFormatter={(value) => value.slice(0, 3)}
+        />
+        <defs>
+          <linearGradient id="fillDesktop" x1="0" y1="0" x2="0" y2="1">
+            <stop
+              offset="5%"
+              stopColor="var(--color-desktop)"
+              stopOpacity={0.8}
+            />
+            <stop
+              offset="95%"
+              stopColor="var(--color-desktop)"
+              stopOpacity={0.1}
+            />
+          </linearGradient>
+        </defs>
+        <Area
+          dataKey="desktop"
+          type="natural"
+          fill="url(#fillDesktop)"
+          fillOpacity={0.4}
+          stroke="var(--color-desktop)"
+          stackId="a"
+          dot={({ cx, cy, payload }) => {
+            const r = 24;
+            return (
+              <GitCommitVertical
+                key={payload.month}
+                x={cx - r / 2}
+                y={cy - r / 2}
+                width={r}
+                height={r}
+                fill="hsl(var(--background))"
+                stroke="var(--color-desktop)"
+              />
+            );
+          }}
+        />
+      </AreaChart>
+    </ChartContainer>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,106 @@
-import { Button } from "@/components/ui/button";
+"use client";
 
-export default function Home() {
+import { Button } from "@/components/ui/button";
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+} from "@/components/ui/card";
+import { Checkbox } from "@/components/ui/checkbox";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
+import {
+  Select,
+  SelectContent,
+  SelectGroup,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "@/components/ui/select";
+import Link from "next/link";
+import { MainChart } from "./main-chart";
+import { useRouter } from "next/navigation";
+
+export default function Home({ searchParams }: { searchParams: any }) {
+  const router = useRouter();
+
+  const test = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+    const params = new URLSearchParams(formData as any);
+    router.push(`?${params.toString()}`);
+  };
+
+  console.log(JSON.stringify(searchParams, null, 2));
+
+  // TODO: parallel routes to display the result
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Button>Test</Button>
-    </main>
+    <>
+      <nav className="flex items-center justify-between p-8">
+        <Link href="/">🎯</Link>
+        <Link href="https://github.com/junqiwu02/rand-sens">⭐ on GitHub</Link>
+      </nav>
+      <main className="flex flex-col items-center justify-between px-24">
+        <Card>
+          <CardHeader>
+            🎯 rand-sens
+            <CardDescription>Get a random sensivity!</CardDescription>
+          </CardHeader>
+          <form onSubmit={test}>
+            <CardContent className="flex flex-col gap-4">
+              <div>
+                <Label htmlFor="dist">Distribution</Label>
+                <Select name="dist" defaultValue="uni" required>
+                  <SelectTrigger id="dist">
+                    <SelectValue />
+                  </SelectTrigger>
+                  <SelectContent>
+                    <SelectGroup>
+                      <SelectItem value="uni">Uniform</SelectItem>
+                      <SelectItem value="norm">Normal</SelectItem>
+                    </SelectGroup>
+                  </SelectContent>
+                </Select>
+              </div>
+
+              <MainChart />
+
+              <div>
+                <Label htmlFor="avg">Average Sens</Label>
+                <Input
+                  id="avg"
+                  type="number"
+                  name="avg"
+                  defaultValue="0.5"
+                  step={0.001}
+                  required
+                />
+                <Label htmlFor="diff">Max Difference</Label>
+                <Input
+                  id="diff"
+                  type="number"
+                  name="diff"
+                  defaultValue="0.5"
+                  step={0.001}
+                  required
+                />
+              </div>
+
+              <div className="flex items-center gap-2">
+                <Checkbox name="bare" id="bare" />
+                <Label htmlFor="bare">Bare-bones Result</Label>
+              </div>
+            </CardContent>
+
+            <CardFooter>
+              <Button>Generate</Button>
+            </CardFooter>
+          </form>
+        </Card>
+      </main>
+    </>
   );
 }
